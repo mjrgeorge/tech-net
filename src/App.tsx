191ba84a4224f714910f14f156716122b36ff0d1
mjrgeorge <mjrgeorge@gmail.com>
2023-07-06 19:20:48 +0600

    redux setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { RootState } from "./redux/store"
+
 function App() {
+  const {count} = useAppSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
 
   return (
-    <h1 className='bg-red-500'>Tech net</h1>
+    <div className="bg-gray-50 ">
+          <button type="button" className="rounded-md bg-green-600 px-3 py-2" onClick={()=>dispatch(increment())}>Increment</button>
+          <button type="button" className="rounded-md bg-yellow-600 px-3 py-2" onClick={()=>dispatch(incrementByAmount(5))}>Increment By Value</button>
+          <div>{count}</div>
+          <button type="button" className="rounded-md bg-red-600 px-3 py-2" onClick={()=>dispatch(decrement())}>Decrement</button>
+        </div>
   )
 }
 
